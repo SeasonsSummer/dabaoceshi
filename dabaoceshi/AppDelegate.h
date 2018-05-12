10294f413a0e38f90b61fd6595d3676ccd4fb79c
赵文旗 <>
2018-05-12 09:12:13 +0800

    111

---
+++ dabaoceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dabaoceshi
+//
+//  Created by 赵文旗 on 2018/5/9.
+//  Copyright © 2018年 赵文旗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dabaoceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dabaoceshi
+//
+//  Created by 赵文旗 on 2018/5/9.
+//  Copyright © 2018年 赵文旗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
